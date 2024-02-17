65b395d7e0595d9351177a24db59f94f1e5ab347
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-02-17 15:05:31 -0500

    build: Configure automatic deployment with GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site:"https://upb-code-labs.github.io", 
+	base: "/manual",
 	integrations: [
 		starlight({
 			title: 'Code Labs manual',
